de27279e938fe89a0268b33afd6dae9bdd25bb6c
m.shakiba <work.shakiba@gmail.com>
2026-06-20 19:43:53 +0330

    added user lesson migrations

---
+++ src/Learnup.Infrastructure/Migrations/20260620161158_UpdatedUserLesson.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Learnup.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedUserLesson : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LastReadEntityId",
+                table: "UserLesson",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LastReadEntityType",
+                table: "UserLesson",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LastVisitedAt",
+                table: "UserLesson",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastReadEntityId",
+                table: "UserLesson");
+
+            migrationBuilder.DropColumn(
+                name: "LastReadEntityType",
+                table: "UserLesson");
+
+            migrationBuilder.DropColumn(
+                name: "LastVisitedAt",
+                table: "UserLesson");
+        }
+    }
+}
