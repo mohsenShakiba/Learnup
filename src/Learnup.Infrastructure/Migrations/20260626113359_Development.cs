d9ca7c34319cdabe4cec814dbff8a183a0c5cca8
m.shakiba <work.shakiba@gmail.com>
2026-06-26 16:53:16 +0330

    added new migration for person

---
+++ src/Learnup.Infrastructure/Migrations/20260626113359_Development.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Learnup.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Development : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
